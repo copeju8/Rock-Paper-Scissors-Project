2a08c71303368dda9f77779dca6f10bc3f3c9a49
MILES\jgeorge <jgeorge@striven.com>
2021-03-06 23:10:30 -0500

    Update rps-project.js

--- rps-project.js
+++ rps-project.js
@@ -1,6 +1,3 @@
-   
-      
-    
     function gameIntro() {
       //window.onload=gameIntro;
       alert("Welcome to Rock paper scissors game! \nStart the game by opening the console (Press F12)\nEnter playGame() in the console to start the game.");
@@ -12,6 +9,7 @@
     }
   
     const items = ["rock", "paper", "scissors"];
+            
     var player;
     var comp;
 
@@ -26,11 +24,20 @@
         var comp = items[num].toLowerCase();
           return comp;
         }
-         
+        
     
-    function compare (choice1, choice2) {         
+    function compare (choice1, choice2) { 
+      
+      let tie = 0;
+      let win = 0;
+      let loss = 0;
+      let playerScore = 0;
+      let computerScore = 0;
+
+      
         if(choice1 === choice2){
             return "The choice is a tie";
+
         }
         else if(choice1 === "rock"){
               if(choice2 === "scissors"){
@@ -58,51 +65,22 @@
     function playGame(){
       
       var rounds = 0;
-      var counter=0;
-      var playerScore = 0;
-      var computerScore = 0;
       var start = gameIntro();
       alert("You and the computer are playing " + start + " rounds.")
       alert("You go first!");
 
         
         console.log("Round: " + rounds++);
+
           var player = playerOne();
           var p1 = player;
           var comp = computer();
           var cp = comp;
           console.log("You selected " + p1 + " and the Computer selected " + cp +"!"); 
           
-          var result =  compare(p1, cp);
+          var result = compare(p1, cp);
           console.log(result);
       
     
-
-
-
-
-      /*var p1 = playerOne();
-      var cp = computer();
-      alert(p1);
-      winner();*/
-      
-      /*if (p1 === items && computer === items){ /*|| 
-              p1 === items && cp === items||
-              p1 === items && cp === items) 
-                return("Result: It is a tie!"); 
-                
-          }   
-          
-           /* else if (player === items[1] && cp === items[3]|| 
-                  p1 === items[2] && cp === items[1]||
-                  p1 === items[3] && cp === items[2]) {
-            {
-              console.log( "tea");
-                  return("Result: Player One won!");         
-            }
-            }else {
-                  //return ("Result: The computer won!"); 
-      }  
-      console.log( "tom");*/
     } 
   
