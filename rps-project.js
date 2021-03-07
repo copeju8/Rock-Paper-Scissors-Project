d93f3f55311cbda0e0a3c6942516700268c40d55
MILES\jgeorge <jgeorge@striven.com>
2021-03-06 21:57:46 -0500

    css footer

--- rps-project.js
+++ rps-project.js
@@ -15,91 +15,65 @@
     var player;
     var comp;
 
-      function playerOne() {  
+    function playerOne() {  
         let player = prompt("Please select one item: rock, paper, or scissor.");      
           if (player.toLowerCase() !=null){
             return  player;
           } 
         }    
-     function computer() {
+    function computer() {
         var num = Math.floor(Math.random() * items.length); 
         var comp = items[num].toLowerCase();
           return comp;
         }
          
     
-      function compare (choice1, choice2) {   
-        //var tie = [];
-        
+    function compare (choice1, choice2) {         
         if(choice1 === choice2){
             return "The choice is a tie";
         }
         else if(choice1 === "rock"){
-
-              if(choice2 = "scissors"){
-
-                return "rock wins!"
+              if(choice2 === "scissors"){
+                return "Rock wins!"
               }
                 else{
-                  return "paper wins"
-        }
-        
-        }
-        else if(choice2 ==="paper"){
+                  return "Paper wins"
+        }  
+
+        }else if(choice2 === "paper"){
                 if(choice2 ==="rock"){
                     return"Paper wins!";
-                }    
-                else {
+                }else {
                     return "scissors wins";
                 }
-
-
         }  
-      }   
+      }    
     
-      
-       
-       
-
-
-
-    /*
-
-    Winner: rock and scissors, rock wins
+    
+   /* Winner: rock and scissors, rock wins
             scissors and paper, scissors wins
-            paper and rock, paper wins
+            paper and rock, paper wins*/
 
-    
-            
-    /*
-
-    /*var input;
-    var rounds = 0;
-    var count = 0;
-    var playerScore = 0;
-    var computerScore = 0;
-    var won;
-    var loss;
-    var tie; */
- 
 
     function playGame(){
-      var start = gameIntro();
+      
       var rounds = 0;
+      var counter=0;
+      var playerScore = 0;
+      var computerScore = 0;
+      var start = gameIntro();
       alert("You and the computer are playing " + start + " rounds.")
       alert("You go first!");
 
-     
-          
-          console.log("Round: " + rounds++);
+        
+        console.log("Round: " + rounds++);
           var player = playerOne();
-          //let player = prompt("Please select one item: rock, paper, or scissor.");  
           var p1 = player;
           var comp = computer();
           var cp = comp;
-          console.log("You selected " + p1 + " and Computer selected " + cp +"!"); 
-          var result =  compare(p1, cp);
+          console.log("You selected " + p1 + " and the Computer selected " + cp +"!"); 
           
+          var result =  compare(p1, cp);
           console.log(result);
       
     
